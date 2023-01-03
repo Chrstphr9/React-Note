b63758c4fc5e0ea8ee30781b0374aa0f2f27cf38
Chstphr <somto565@gmail.com>
2023-01-03 18:38:29 +0100

    added typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Typography from '@mui/material/Typography';
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography 
+        variant='h6'
+        component='h2'
+        gutterBottom
+        color='textSecondary'
+        >
+        Create A New Note
+      </Typography>
     </div>
   )
 }
